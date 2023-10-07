716e4ea2f7fcfe294a1637b6baa7bb100f4b7b22
stceel <stceel1@gmail.com>
2023-10-07 13:50:59 -0500

    app changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ function App() {
   return (
     <>
       <h1>Test</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, repudiandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, repudiandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, repudiandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, repudiandae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, repudiandae?</p>
     </>
   )
 }
